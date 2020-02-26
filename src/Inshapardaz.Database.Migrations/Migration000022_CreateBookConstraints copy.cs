612ec282318cee925a5bf0e385c98664a6aeb9b2
Omar Faruk <omar.faruk@outlook.com>
2020-02-26 11:51:29 +0000

    Added Book to Library

---
+++ src/Inshapardaz.Database.Migrations/Migration000022_CreateBookConstraints copy.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000024)]
+    public class Migration000024_CreateBookToLibrary : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Book).InSchema(Schemas.Library)
+                 .AddColumn("LibraryId")
+                 .AsInt32();
+            
+            Create.ForeignKey("FK_Book_Library")
+                  .FromTable(Tables.Book).InSchema(Schemas.Library).ForeignColumn("LibraryId")
+                  .ToTable(Tables.Library).InSchema(Schemas.Library).PrimaryColumn(Columns.Id)
+                  .OnDelete(System.Data.Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Book_Library").OnTable(Tables.Book).InSchema(Schemas.Library);
+            Delete.Column("LibraryId").FromTable(Tables.Book).InSchema(Schemas.Library);
+        }
+    }
+}
