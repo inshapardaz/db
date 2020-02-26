d804b1557a9249c5b78499b89cc493fc573efa22
Omar Faruk <omar.faruk@outlook.com>
2020-02-26 10:00:20 +0000

    Added series contraints

---
+++ src/Inshapardaz.Database.Migrations/Migration000021_CreateSeriesConstraints.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000021)]
+    public class Migration000021_CreateSeriesConstraints : Migration
+    {
+        public override void Up()
+        {
+            Alter.Column(Columns.Name).OnTable(Tables.Series).InSchema(Schemas.Library)
+                 .AsString(256);
+            Alter.Column(Columns.Name).OnTable(Tables.Series).InSchema(Schemas.Library)
+                 .AsString(256)
+                 .Indexed("IDX_Series_Name");
+
+            Create.ForeignKey("FK_Series_Image")
+                  .FromTable(Tables.Series).InSchema(Schemas.Library).ForeignColumn(Columns.ImageId)
+                  .ToTable(Tables.File).InSchema(Schemas.Inshapardaz).PrimaryColumn(Columns.Id)
+                  .OnDelete(System.Data.Rule.SetDefault);
+        }
+
+        public override void Down()
+        {
+            Delete.Index("IDX_Series_Name").OnTable(Tables.Series).InSchema(Schemas.Library)
+                  .OnColumn("Name");
+
+            Alter.Column(Columns.Name).OnTable(Tables.Series).InSchema(Schemas.Library)
+                 .AsString(int.MaxValue);
+
+            Delete.ForeignKey("FK_Series_Image").OnTable(Tables.Series).InSchema(Schemas.Library);
+        }
+    }
+}
