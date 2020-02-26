5aed17f828e48732ec0a02c10791746162f2ac47
Omar Faruk <omar.faruk@outlook.com>
2020-02-26 12:03:48 +0000

    Added periodicals to library

---
+++ src/Inshapardaz.Database.Migrations/Migration000026_AddPeriodicalToLibrary.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000026)]
+    public class Migration000026_AddPeriodicalToLibrary : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Periodical).InSchema(Schemas.Library)
+                 .AddColumn("LibraryId")
+                 .AsInt32()
+                 .Nullable();
+            
+            Create.ForeignKey("FK_Periodical_Library")
+                  .FromTable(Tables.Periodical).InSchema(Schemas.Library).ForeignColumn("LibraryId")
+                  .ToTable(Tables.Library).InSchema(Schemas.Library).PrimaryColumn(Columns.Id)
+                  .OnDelete(System.Data.Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_Periodical_Library").OnTable(Tables.Periodical).InSchema(Schemas.Library);
+            Delete.Column("LibraryId").FromTable(Tables.Periodical).InSchema(Schemas.Library);
+        }
+    }
+}
