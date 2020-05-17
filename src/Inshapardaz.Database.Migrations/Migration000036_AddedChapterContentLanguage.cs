33039f069aff648689611e73f68a0a466952084c
Omar Faruk <omar.faruk@outlook.com>
2020-05-17 04:40:38 +0100

    Added chapter content language

---
+++ src/Inshapardaz.Database.Migrations/Migration000036_AddedChapterContentLanguage.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000036)]
+    public class Migration000036_AddedChapterContentLanguage : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.ChapterContent).InSchema(Schemas.Library)
+                .AddColumn("Language").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Language")
+                .FromTable(Tables.ChapterContent).InSchema(Schemas.Library);
+        }
+    }
+}
