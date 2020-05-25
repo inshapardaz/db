532f843330104f1f7a3dfc64908fe5eeaf05a9fe
Omar Faruk <omar.faruk@outlook.com>
2020-05-25 14:29:04 +0100

    Removed mimetype on chapter content

---
+++ src/Inshapardaz.Database.Migrations/Migration000036_RemoveChapterContentMimeType.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000037)]
+    public class Migration000036_RemoveChapterContentMimeType : Migration
+    {
+        public override void Up()
+        {
+            Delete.Column("MimeType").FromTable(Tables.ChapterContent).InSchema(Schemas.Library);
+        }
+
+        public override void Down()
+        {
+            Alter.Table(Tables.ChapterContent).InSchema(Schemas.Library)
+                .AddColumn("MimeType").AsString(int.MaxValue).Nullable();
+        }
+    }
+}
