a53c4dceb0997d9cad4b2dacc8e5e613908f6777
Omar Faruk <omar.faruk@outlook.com>
2020-07-07 12:31:01 +0100

    Added book index

---
+++ src/Inshapardaz.Database.Migrations/Migration000040_AddBookIndex.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System.IO;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000040)]
+    public class Migration000040_AddBookIndex : Migration
+    {
+        public override void Up()
+        {
+            Create.Index("IDX_Book_1")
+                .OnTable(Tables.Book)
+                .OnColumn("AuthorId").Ascending()
+                .OnColumn("IsPublic").Ascending()
+                .OnColumn("SeriesId").Ascending()
+                .WithOptions().NonClustered();
+        }
+
+        public override void Down()
+        {
+            Delete.Index("IDX_Book_1").OnTable(Tables.Book);
+        }
+    }
+}
