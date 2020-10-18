ada215b16e01d9b362e382970606306443ca7257
Omar Faruk <omar.faruk@outlook.com>
2020-10-18 23:38:14 +0100

    Changed book language to be string

---
+++ src/Inshapardaz.Database.Migrations/Migration000045_UpdatedLanguageToBeString.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000045)]
+    public class Migration000045_UpdatedLanguageToBeString : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Book)
+            .InSchema(Schemas.Dbo)
+            .AlterColumn(Columns.Language).AsString(10);
+        }
+
+        public override void Down()
+        {
+            Alter.Table(Tables.Book)
+            .InSchema(Schemas.Dbo)
+            .AlterColumn(Columns.Language).AsInt32();
+        }
+    }
+}
