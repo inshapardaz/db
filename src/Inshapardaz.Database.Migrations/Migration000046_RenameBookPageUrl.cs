07c6ceec5930ab852d09bbc9af88f58e8c043a39
Omar Faruk <omar.faruk@outlook.com>
2020-11-01 18:28:45 +0000

    Rename book page  url column

---
+++ src/Inshapardaz.Database.Migrations/Migration000046_RenameBookPageUrl.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000046)]
+    public class Migration000046_RenameBookPageUrl : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("PageUrl").OnTable(Tables.BookPage).To(Columns.ImageId);
+        }
+
+        public override void Down()
+        {
+            Rename.Column(Columns.ImageId).OnTable(Tables.BookPage).To("PageUrl");
+        }
+    }
+}
