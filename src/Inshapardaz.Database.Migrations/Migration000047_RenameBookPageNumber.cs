bfe292e975ba2406b2386ca560e8332d877dcd77
Omar Faruk <omar.faruk@outlook.com>
2020-11-05 22:57:00 +0000

    Renamed the page number column

---
+++ src/Inshapardaz.Database.Migrations/Migration000047_RenameBookPageNumber.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000047)]
+    public class Migration000047_RenameBookPageNumber : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("PageNumber").OnTable(Tables.BookPage).To("SequenceNumber");
+        }
+
+        public override void Down()
+        {
+            Rename.Column("SequenceNumber").OnTable(Tables.BookPage).To("PageNumber");
+        }
+    }
+}
