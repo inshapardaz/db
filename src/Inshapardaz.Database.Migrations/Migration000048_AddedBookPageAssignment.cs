073374daeecc36c1b13d5bb54bb705d9382c48d1
Omar Faruk <omar.faruk@outlook.com>
2020-11-06 06:38:37 +0000

    Added book Page assign columns

---
+++ src/Inshapardaz.Database.Migrations/Migration000048_AddedBookPageAssignment.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000048)]
+    public class Migration000048_AddedBookPageAssignment : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.BookPage).
+                InSchema(Schemas.Dbo)
+                .AddColumn("Status").AsInt32().WithDefaultValue(0)
+                .AddColumn("UserId").AsGuid().Nullable()
+                .AddColumn("AssignTimeStamp").AsDateTime2().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Status").FromTable(Tables.BookPage).InSchema(Schemas.Dbo);
+            Delete.Column("UserId").FromTable(Tables.BookPage).InSchema(Schemas.Dbo);
+            Delete.Column("AssignTimeStamp").FromTable(Tables.BookPage).InSchema(Schemas.Dbo);
+        }
+    }
+}
