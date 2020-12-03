d85ba92360c5e3999532c80c7c2cf0936bb8c5ee
Omar Faruk <omar.faruk@outlook.com>
2020-12-03 01:18:32 +0000

    Renamed userid to accountid

---
+++ src/Inshapardaz.Database.Migrations/Migration000052_AddedAccountForeignKeys.cs
@@ -0,0 +1,59 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000052)]
+    public class Migration000052_AddedAccountForeignKeys : Migration
+    {
+        public override void Up()
+        {
+            Rename.Column("UserId").OnTable(Tables.RecentBooks).InSchema(Schemas.Dbo)
+                  .To("AccountId");
+            Create.ForeignKey("FK_RecentBooks_Accounts_AccountId")
+                .FromTable(Tables.RecentBooks).InSchema(Schemas.Dbo).ForeignColumn("AccountId")
+                .ToTable("Accounts").InSchema(Schemas.Dbo).PrimaryColumn(Columns.Id)
+                .OnDelete(System.Data.Rule.Cascade);
+
+            Rename.Column("UserId").OnTable(Tables.FavoriteBooks).InSchema(Schemas.Dbo)
+                  .To("AccountId");
+            Create.ForeignKey("FK_FavoriteBooks_Accounts_AccountId")
+                .FromTable(Tables.FavoriteBooks).InSchema(Schemas.Dbo).ForeignColumn("AccountId")
+                .ToTable("Accounts").InSchema(Schemas.Dbo).PrimaryColumn(Columns.Id)
+                .OnDelete(System.Data.Rule.Cascade);
+
+            Rename.Column("UserId").OnTable(Tables.BookShelf).InSchema(Schemas.Dbo)
+                             .To("AccountId");
+
+            Create.ForeignKey("FK_BookShelf_Accounts_AccountId")
+                .FromTable(Tables.BookShelf).InSchema(Schemas.Dbo).ForeignColumn("AccountId")
+                .ToTable("Accounts").InSchema(Schemas.Dbo).PrimaryColumn(Columns.Id)
+                .OnDelete(System.Data.Rule.Cascade);
+
+
+            Rename.Column("UserId").OnTable(Tables.BookPage).InSchema(Schemas.Dbo)
+                             .To("AccountId");
+
+            Create.ForeignKey("FK_BookPage_Accounts_AccountId")
+                .FromTable(Tables.BookPage).InSchema(Schemas.Dbo).ForeignColumn("AccountId")
+                .ToTable("Accounts").InSchema(Schemas.Dbo).PrimaryColumn(Columns.Id)
+                .OnDelete(System.Data.Rule.Cascade);
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey("FK_RecentBooks_Accounts_AccountId").OnTable(Tables.RecentBooks).InSchema(Schemas.Dbo);
+            Delete.ForeignKey("FK_FavoriteBooks_Accounts_AccountId").OnTable(Tables.FavoriteBooks).InSchema(Schemas.Dbo);
+            Delete.ForeignKey("FK_BookShelf_Accounts_AccountId").OnTable(Tables.BookShelf).InSchema(Schemas.Dbo);
+            Delete.ForeignKey("FK_BookPage_Accounts_AccountId").OnTable(Tables.BookPage).InSchema(Schemas.Dbo);
+
+            Rename.Column("AccountId").OnTable(Tables.RecentBooks).InSchema(Schemas.Dbo)
+                 .To("UserId");
+            Rename.Column("AccountId").OnTable(Tables.FavoriteBooks).InSchema(Schemas.Dbo)
+            .To("UserId");
+            Rename.Column("AccountId").OnTable(Tables.BookShelf).InSchema(Schemas.Dbo)
+                 .To("UserId");
+            Rename.Column("AccountId").OnTable(Tables.BookPage).InSchema(Schemas.Dbo)
+            .To("UserId");
+        }
+    }
+}
