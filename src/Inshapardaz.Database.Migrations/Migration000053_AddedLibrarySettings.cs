e169e99101395de47499b97577252d7f220a5fbb
Omar Faruk <omar.faruk@outlook.com>
2020-12-09 11:57:42 +0000

    Added LibrarySettins

---
+++ src/Inshapardaz.Database.Migrations/Migration000053_AddedLibrarySettings.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000053)]
+    public class Migration000053_AddedLibrarySettings : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Library).InSchema(Schemas.Dbo)
+                .AddColumn("PrimaryColor").AsString().Nullable()
+                .AddColumn("SecondaryColor").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("PrimaryColor").FromTable(Tables.Library).InSchema(Schemas.Dbo);
+            Delete.Column("SecondaryColor") .FromTable(Tables.Library).InSchema(Schemas.Dbo);
+        }
+    }
+}
