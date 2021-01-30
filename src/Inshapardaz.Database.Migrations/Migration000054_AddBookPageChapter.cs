29689e972cb62f1bcb20df3468492e588bf8e087
Omar Faruk <omar.faruk@outlook.com>
2021-01-30 22:47:34 +0000

    Added bookpage chapter

---
+++ src/Inshapardaz.Database.Migrations/Migration000054_AddBookPageChapter.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000054)]
+    public class Migration000054_AddBookPageChapter : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.BookPage).InSchema(Schemas.Dbo)
+                .AddColumn("ChapterId").AsInt32().Nullable();
+
+            Create.ForeignKey("FK_BookPage_Chapter_ChapterId")
+            .FromTable(Tables.BookPage).InSchema(Schemas.Dbo).ForeignColumn("ChapterId")
+            .ToTable(Tables.Chapter).InSchema(Schemas.Dbo).PrimaryColumn(Columns.Id)
+            .OnDelete(System.Data.Rule.SetNull);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("ChapterId").FromTable(Tables.BookPage).InSchema(Schemas.Dbo);
+        }
+    }
+}
