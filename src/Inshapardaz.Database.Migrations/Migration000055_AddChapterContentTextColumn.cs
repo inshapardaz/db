25de3255617ab983304c763ff31d85c8f0a7d78e
Omar Faruk <omar.faruk@outlook.com>
2021-02-03 00:03:25 +0000

    Added Chapter Text Column

---
+++ src/Inshapardaz.Database.Migrations/Migration000055_AddChapterContentTextColumn.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000055)]
+    public class Migration000055_AddChapterContentTextColumn : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.ChapterContent).InSchema(Schemas.Dbo)
+                .AddColumn("Text").AsString(int.MaxValue).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Text").FromTable(Tables.ChapterContent).InSchema(Schemas.Dbo);
+        }
+    }
+}
