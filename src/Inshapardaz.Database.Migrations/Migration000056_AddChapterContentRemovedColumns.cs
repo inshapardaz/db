4d8416d4431e49c7d4ba65e64fdc1a0c1034186e
Omar Faruk <omar.faruk@outlook.com>
2021-02-05 22:43:04 +0000

    Removed unused columns from chaptercontent and added unique

---
+++ src/Inshapardaz.Database.Migrations/Migration000056_AddChapterContentRemovedColumns.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000056)]
+    public class Migration000056_AddChapterContentRemovedColumns : Migration
+    {
+        public override void Up()
+        {
+            Delete.ForeignKey("FK_ChapterContent_File_FileId")
+                           .OnTable(Tables.ChapterContent).InSchema(Schemas.Dbo);
+            Delete.Column("ContentUrl").FromTable(Tables.ChapterContent).InSchema(Schemas.Dbo);
+            Delete.Column("FileId").FromTable(Tables.ChapterContent).InSchema(Schemas.Dbo);
+
+            Create.UniqueConstraint("UQ_ChapterId_Language")
+                  .OnTable(Tables.ChapterContent).Columns("ChapterId", "Language");
+        }
+
+        public override void Down()
+        {
+            Alter.Table(Tables.ChapterContent).InSchema(Schemas.Dbo)
+                .AddColumn("ContentUrl").AsString(int.MaxValue).Nullable();
+            Alter.Table(Tables.ChapterContent).InSchema(Schemas.Dbo)
+                .AddColumn("FileId").AsInt32().Nullable();
+
+            Delete.UniqueConstraint("UQ_ChapterId_Language").FromTable(Tables.ChapterContent).InSchema(Schemas.Dbo);
+        }
+    }
+}
