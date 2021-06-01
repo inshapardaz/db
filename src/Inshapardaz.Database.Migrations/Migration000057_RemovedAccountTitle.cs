4bcf0bbd769b01d605ac4fddbaf85ad7226056d5
Omar Faruk <omar.faruk@outlook.com>
2021-06-01 22:25:05 +0100

    Removed account title

---
+++ src/Inshapardaz.Database.Migrations/Migration000057_RemovedAccountTitle.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000057)]
+    public class Migration000057_RemovedAccountTitle : Migration
+    {
+        public override void Up()
+        {
+            Delete.Column("Title").FromTable("Accounts").InSchema(Schemas.Dbo);
+        }
+
+        public override void Down()
+        {
+            Alter.Table("Accounts").InSchema(Schemas.Dbo)
+                .AddColumn("Title").AsString().Nullable();
+        }
+    }
+}
