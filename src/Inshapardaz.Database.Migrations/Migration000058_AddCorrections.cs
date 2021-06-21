6527425dc563122ebe8a0888c5fbdc48d8d60f83
Omar Faruk <omar.faruk@outlook.com>
2021-06-21 22:21:40 +0100

    Added corrections

---
+++ src/Inshapardaz.Database.Migrations/Migration000058_AddCorrections.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000058)]
+    public class Migration000058_AddCorrections : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Corrections").InSchema(Schemas.Dbo)
+                .WithColumn("Id").AsInt32().Identity().PrimaryKey()
+                .WithColumn("language").AsString(10).Indexed()
+                .WithColumn("phrase").AsString(50)
+                .WithColumn("replacement").AsString(50);
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Corrections").InSchema(Schemas.Dbo);
+        }
+    }
+}
