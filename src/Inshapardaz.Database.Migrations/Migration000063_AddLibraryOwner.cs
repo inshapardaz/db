4c367d1faaa14108f4a5cea0777943482093ac68
Omar Faruk <omar.faruk@outlook.com>
2021-10-06 22:27:56 +0100

    Added library owner email

---
+++ src/Inshapardaz.Database.Migrations/Migration000063_AddLibraryOwner.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000063)]
+    public class Migration000063_AddLibraryOwner : Migration
+    {
+        public override void Up()
+        {
+             Alter.Table("Library").InSchema(Schemas.Dbo)
+                .AddColumn("OwnerEmail").AsString().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("OwnerEmail").FromTable("Library").InSchema(Schemas.Dbo);
+
+        }
+    }
+}
