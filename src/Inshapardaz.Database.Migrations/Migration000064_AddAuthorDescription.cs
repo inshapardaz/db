78d861a2dbcd52d850b5da5e472fd79280173177
Omar Faruk <omar.faruk@outlook.com>
2021-10-17 13:44:26 +0100

    Added author description

---
+++ src/Inshapardaz.Database.Migrations/Migration000064_AddAuthorDescription.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000064)]
+    public class Migration000064_AddAuthorDescription : Migration
+    {
+        public override void Up()
+        {
+             Alter.Table("Author").InSchema(Schemas.Dbo)
+                .AddColumn("Description").AsString(int.MaxValue).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Description").FromTable("Author").InSchema(Schemas.Dbo);
+        }
+    }
+}
