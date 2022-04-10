35613a7923f24b25e56c4f0ad44bdedf8a2147a4
Omar Faruk <omar.faruk@outlook.com>
2022-04-10 14:44:13 +0100

    Added author type column

---
+++ src/Inshapardaz.Database.Migrations/Migration000069_AddAuthorType.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000069)]
+    public class Migration000069_AddAuthorType : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Author).InSchema(Schemas.Dbo)
+                .AddColumn("AuthorType").AsInt32().WithDefaultValue(0).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("AuthorType").FromTable(Tables.Author).InSchema(Schemas.Dbo);
+        }
+    }
+}
