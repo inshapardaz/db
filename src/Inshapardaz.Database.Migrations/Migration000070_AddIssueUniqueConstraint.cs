f094bd6bda44ca0a215fe4fa380f7e9d25ccbf4e
Omar Faruk <omar.faruk@outlook.com>
2022-04-23 15:12:02 +0100

    Added unique contraint on the issue

---
+++ src/Inshapardaz.Database.Migrations/Migration000070_AddIssueUniqueConstraint.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000070)]
+    public class Migration000070_AddIssueUniqueConstraint: Migration
+    {
+        public override void Up()
+        {
+            Create.UniqueConstraint("UNQ_VOLUME_ISSUE")
+                .OnTable(Tables.Issue)
+                .WithSchema(Schemas.Dbo)
+                .Columns("PeriodicalId","Volumenumber","IssueNumber");
+        }
+
+        public override void Down()
+        {
+            Delete.UniqueConstraint("UNQ_VOLUME_ISSUE")
+                .FromTable(Tables.Issue)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
