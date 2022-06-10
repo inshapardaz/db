d4c663bdaa6b94767fc835ac8c4d4fc326a2991f
Omar Faruk <omar.faruk@outlook.com>
2022-06-10 11:46:37 +0100

    Added Chapter editing statuses

---
+++ src/Inshapardaz.Database.Migrations/Migration000071_AddedChapterAssignment.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000071)]
+    public class Migration000071_AddedChapterAssignment : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Chapter).
+                InSchema(Schemas.Dbo)
+                .AddColumn("Status").AsInt32().WithDefaultValue(0)
+                .AddColumn("WriterAccountId").AsGuid().Nullable()
+                .AddColumn("WriterAssignTimeStamp").AsDateTime2().Nullable()
+                .AddColumn("ReviewerAccountId").AsInt32().Nullable()
+                .AddColumn("ReviewerAssignTimeStamp").AsDateTime2().Nullable();
+            
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Status").FromTable(Tables.Chapter).InSchema(Schemas.Dbo);
+            Delete.Column("WriterAccountId").FromTable(Tables.Chapter).InSchema(Schemas.Dbo);
+            Delete.Column("WriterAssignTimeStamp").FromTable(Tables.Chapter).InSchema(Schemas.Dbo);
+            Delete.Column("ReviewerAccountId").FromTable(Tables.Chapter).InSchema(Schemas.Dbo);
+            Delete.Column("ReviewerAssignTimeStamp").FromTable(Tables.Chapter).InSchema(Schemas.Dbo);
+        }
+    }
+}
