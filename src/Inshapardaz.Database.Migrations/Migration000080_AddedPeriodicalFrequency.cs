2c95457fa30174e2c57eb8496df5a332d2ed066c
Omar Faruk <omar.faruk@outlook.com>
2022-09-04 13:17:42 +0100

    Added periodical frequency

---
+++ src/Inshapardaz.Database.Migrations/Migration000080_AddedPeriodicalFrequency.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000080)]
+    public class Migration000080_AddedPeriodicalFrequency : Migration
+    {
+        public override void Up()
+        {
+            Create.Column("Frequency")
+                  .OnTable("Periodical").InSchema(Schemas.Dbo)
+                  .AsInt32().WithDefaultValue(0);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Frequency").FromTable("Periodical").InSchema(Schemas.Dbo);        }
+    }
+}
