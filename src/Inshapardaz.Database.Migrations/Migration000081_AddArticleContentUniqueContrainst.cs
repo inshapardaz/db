49ad14a7c653709f56333eed6b46afd1c81dd1f0
Omar Faruk <omar.faruk@outlook.com>
2022-10-09 08:26:39 +0100

    Added Unique constraint on article content

---
+++ src/Inshapardaz.Database.Migrations/Migration000081_AddArticleContentUniqueContrainst.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000081)]
+    public class Migration000081_AddArticleContentUniqueContrainst : Migration
+    {
+        public override void Up()
+        {
+            Create.UniqueConstraint("UQ_ArticleId_Language")
+                  .OnTable(Tables.ArticleContent).Columns("ArticleId", "Language");
+        }
+
+        public override void Down()
+        {
+            Delete.UniqueConstraint("UQ_ArticleId_Language")
+                .FromTable(Tables.ArticleContent)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
