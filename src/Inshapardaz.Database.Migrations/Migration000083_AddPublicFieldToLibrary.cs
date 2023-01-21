edd86d59fca868ffff442a5adc97062eb6ea02c8
Omar Faruk <omar.faruk@outlook.com>
2023-01-21 14:45:44 +0000

    Added Library Public Flag

---
+++ src/Inshapardaz.Database.Migrations/Migration000083_AddPublicFieldToLibrary.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000083)]
+    public class Migration000083_AddPublicFieldToLibrary : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Library).InSchema(Schemas.Dbo)
+                .AddColumn("Public")
+                .AsBoolean()
+                .WithDefaultValue(0);
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Public")
+                .FromTable(Tables.Library)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
