0cbe54ac3b09ccefc280355f86c1991c4ebefb23
Omar Faruk <omar.faruk@outlook.com>
2023-05-12 09:03:29 +0100

    Added Library settings

---
+++ src/Inshapardaz.Database.Migrations/Migration000084_AddLibrarySettings.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000084)]
+    public class Migration000084_AddLibrarySettings : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Library).InSchema(Schemas.Dbo)
+                .AddColumn("DatabaseConnection")
+                .AsString().Nullable();
+            Alter.Table(Tables.Library).InSchema(Schemas.Dbo)
+                .AddColumn("FileStoreType")
+                .AsString().WithDefaultValue("Database");
+            Alter.Table(Tables.Library).InSchema(Schemas.Dbo)
+                .AddColumn("FileStoreSource")
+                .AsString();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("DatabaseConnection")
+                .FromTable(Tables.Library)
+                .InSchema(Schemas.Dbo);
+            Delete.Column("FileStoreType")
+                .FromTable(Tables.Library)
+                .InSchema(Schemas.Dbo);
+            Delete.Column("FileStoreSource")
+            .FromTable(Tables.Library)
+            .InSchema(Schemas.Dbo);
+        }
+    }
+}
