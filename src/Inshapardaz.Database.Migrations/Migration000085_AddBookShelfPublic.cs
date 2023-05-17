c4cd3ec46b186b6b975fb424fc1bf0ef0ab23508
Omar Faruk <omar.faruk@outlook.com>
2023-05-17 09:33:44 +0100

    Added more columns to BookShelf

---
+++ src/Inshapardaz.Database.Migrations/Migration000085_AddBookShelfPublic.cs
@@ -0,0 +1,34 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000085)]
+    public class Migration000085_AddBookShelfPublic : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.BookShelf).InSchema(Schemas.Dbo)
+                .AddColumn(Columns.Description)
+                .AsString().Nullable()
+                .AddColumn(Columns.IsPublic)
+                .AsBoolean().Nullable()
+                .AddColumn(Columns.ImageId)
+                .AsInt32().Nullable(); ;
+            Alter.Table(Tables.BookShelfBook).InSchema(Schemas.Dbo)
+                .AddColumn("Index")
+                .AsInt32().Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column(Columns.IsPublic)
+                .Column(Columns.Description)
+                .Column(Columns.ImageId)
+                .FromTable(Tables.BookShelf)
+                .InSchema(Schemas.Dbo);
+            Delete.Column("Index")
+                .FromTable(Tables.BookShelfBook)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
