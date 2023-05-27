3bf1f2acce8513145df7a7a058359d99af7e2705
Omar Faruk <omar.faruk@outlook.com>
2023-05-27 23:58:38 +0100

    Added book Source

---
+++ src/Inshapardaz.Database.Migrations/Migration000086_AddBookSource.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000086)]
+    public class Migration000086_AddBookSource : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Book).InSchema(Schemas.Dbo)
+                .AddColumn("Source")
+                .AsString()
+                .Indexed("IDX_Book_Source")
+                .Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Index("IDX_Book_Source")
+                  .OnTable(Tables.Book)
+                  .InSchema(Schemas.Dbo)
+                  .OnColumn("Source");
+            Delete.Column("Source")
+                .FromTable(Tables.Book)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
