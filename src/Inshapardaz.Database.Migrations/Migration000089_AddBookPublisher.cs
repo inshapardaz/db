9a8a8c16d846adeec1eab3693a92d82d20f9add5
Umer Farooq <omar.faruk@outlook.com>
2023-08-20 14:01:10 +0100

    Added book publisher

---
+++ src/Inshapardaz.Database.Migrations/Migration000089_AddBookPublisher.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000089)]
+    public class Migration000089_AddBookPublisher : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table(Tables.Book).InSchema(Schemas.Dbo)
+                .AddColumn("Publisher")
+                .AsString()
+                .Indexed("IDX_Book_Publisher")
+                .Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Index("IDX_Book_Publisher")
+                  .OnTable(Tables.Book)
+                  .InSchema(Schemas.Dbo)
+                  .OnColumn("Publisher");
+            Delete.Column("Publisher")
+                .FromTable(Tables.Book)
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
