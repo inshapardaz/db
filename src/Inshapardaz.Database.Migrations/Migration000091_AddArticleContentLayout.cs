caef7e945c1a2395b65a0a32f3f9a76e314e41ad
Omar Faruk <omar.faruk@outlook.com>
2023-11-15 09:33:47 +0000

    Added article content layout

---
+++ src/Inshapardaz.Database.Migrations/Migration000091_AddArticleContentLayout.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace Inshapardaz.Database.Migrations
+{
+    [Migration(000091)]
+    public class Migration000091_AddArticleContentLayout : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("ArticleContent").InSchema(Schemas.Dbo)
+                .AddColumn("Layout")
+                .AsString()
+                .Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Column("Layout")
+                .FromTable("ArticleContent")
+                .InSchema(Schemas.Dbo);
+        }
+    }
+}
